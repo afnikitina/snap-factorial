5b4789a95c3c50a9aecdb5aa155e46fd5ecd5ef3
Asya Nikitina <asya_nikitina@hotmail.com>
2018-07-23 08:20:27 -0600

    Added the second function to factorial.php

--- php/factorial.php
+++ php/factorial.php
@@ -1,6 +1,9 @@
 <?php
 
-function calcFactorial: int (int $num) {
+function calcFactorial ($num) {
+	if (!is_int($num)) {
+		$num = (int)$num;
+	}
 	$result = $num;
 	for ($i = $num-1; $i >=1; $i--) {
 		$result *= $i;
@@ -9,9 +12,13 @@ function calcFactorial: int (int $num) {
 }
 
 // creating a function that recurrently calls itself whille the condition holds true
-function calcFactorial2 : int (int $num) {
+function calcFactorial2 ($num) {
+	if (!is_int($num)) {
+		$num = (int)$num;
+	}
 	while ($num > 1) {
 		return ($num * ($num-1));
 	}
+	return $num;
 }
 
